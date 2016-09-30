dc33d87c35fe217dca458cf357d47cdf0fcba615
Ari Bouius <abouius@rent.com>
2016-09-30 11:09:52 -0700

    [AB][131457441] Remove lazy handling of session cookies

--- test/reducers/Session.js
+++ test/reducers/Session.js
@@ -5,46 +5,50 @@ import SessionReducer from '../../src/reducers/Session'
 describe('SessionReducer', function() {
   beforeEach(function() {
     this.jsdom = require('jsdom-global')()
-
-    const reducer = new SessionReducer()
-    this.reduce = reducer.reduce.bind(reducer)
-
-    sinon.stub(reducer, 'requestId').returns('123')
+    this.stub = sinon.stub(SessionReducer.prototype, 'requestId').returns('123')
   })
 
   afterEach(function() {
     this.jsdom()
+    this.stub.restore()
   })
 
   describe('#reduce', function() {
     it('assigns a visitor ID', function() {
-      expect(this.reduce().visitor_id).to.exist
+      const reducer = new SessionReducer()
+      expect(reducer.reduce().visitor_id).to.exist
     })
 
     it('assigns a session ID', function() {
-      expect(this.reduce().session_id).to.exist
+      const reducer = new SessionReducer()
+      expect(reducer.reduce().session_id).to.exist
     })
 
     it('assigns a visit ID', function() {
-      expect(this.reduce().visit_id).to.exist
+      const reducer = new SessionReducer()
+      expect(reducer.reduce().visit_id).to.exist
     })
 
     it('uses a visitor ID stored in cookies', function() {
       document.cookie = 'rp_visitor_id=abc'
-      expect(this.reduce().visitor_id).to.equal('abc')
+      const reducer = new SessionReducer()
+      expect(reducer.reduce().visitor_id).to.equal('abc')
     })
 
     it('uses a session ID stored in cookies', function() {
       document.cookie = 'rp_session_id=abc'
-      expect(this.reduce().session_id).to.equal('abc')
+      const reducer = new SessionReducer()
+      expect(reducer.reduce().session_id).to.equal('abc')
     })
 
     it('generates a new visitor ID if one is not found in cookies', function() {
-      expect(this.reduce().session_id).to.equal('123')
+      const reducer = new SessionReducer()
+      expect(reducer.reduce().session_id).to.equal('123')
     })
 
     it('generates a new session ID if one is not found in cookies', function() {
-      expect(this.reduce().session_id).to.equal('123')
+      const reducer = new SessionReducer()
+      expect(reducer.reduce().session_id).to.equal('123')
     })
   })
 })
